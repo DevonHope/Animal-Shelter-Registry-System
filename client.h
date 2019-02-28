7f4439a4ee36ba4c7a4e0952834a103001e84add
WalterMitty2112 <devonhope20@gmail.com>
2019-02-27 21:45:30 -0500

    Final client/animal classes

--- client.h
+++ client.h
@@ -1,28 +1,30 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+#ifndef CLIENT_H
+#define CLIENT_H
 
 using namespace std;
 #include <string>
 
-class Animal
-{
-    public:
-        Animal(string n, int a, string b, string t, int w, int fl, string cp, bool c, bool sh, string g, string co, string ff, int in, int ag, int cour, int pl, int stren, string specs, int kf, int cs, int ht, int cu);
-        ~Animal();
-        string toString() const;
-        
-        string getName() const;
+class Client {
+
+	public:
+		Client(string n, int a, string g, string add, string cl, int pnum, string ty, string bre, bool clws, bool shd, int intel, int ag, int cour, int pl, int stren, string sskill, int kf, int cs, int ht, int tru, int cur, int clen, bool HF);
+		~Client();
+		string toString() const;
+		bool verifyAtt();
+
+		//getters
+		string getName() const;
         int getAge() const;
+        string getAddress() const;
+        int getPNum() const;
         string getBreed() const;
         string getType() const;
-        int getWeight() const;
         bool getHasFur() const;
-        string getClimatePref() const;
+        string getClimate() const;
         bool getClaws() const;
         bool getSheds() const;
         string getGender() const;
         string getColour() const;
-        string getFF() const; //favfood
         int getIntell() const;
         int getAggro() const;
         int getCourage() const;
@@ -35,19 +37,19 @@ class Animal
         int getTrust() const; 
         int getCuriosity() const;
 
-        //setters
-        void setName(string);
+		//setters
+		void setName(string);
         void setAge(int);
+        void setAddress(string);
+        void setPNum(int);
         void setBreed(string);
         void setType(string);
-        void setWeight(int);
         void setHasFur(int);
-        void setClimatePref(string);
+        void setClimate(string);
         void setClaws(bool);
         void setSheds(bool);
         void setGender(string);
         void setColour(string);
-        void setfavFood(string);
         void setIntell(int);
         void setAggro(int);
         void setCourage(int);
@@ -60,35 +62,33 @@ class Animal
         void setTrust(int);
         void setCuriosity(int);
 
+	protected:
+		string type;
+		string breed;
+		bool claws;
+		bool sheds;
+		bool hasFur;
+		int intell;
+		int aggro;
+		int courage;
+		int play;
+		int strength;
+		string specialSkill;
+		int kidFriendly;
+		int commSkills;
+		int houseTrained;
+		int trust;
+		int curiosity;
+		int clean;
+		bool hasFur;
 
-    private:
-        string name;
-        int age;
-        int weight;
-        string type;
-        string gender;
-        string colour;
-        string breed;
-        bool hasFur;
-        string climatePref;
-        bool claws;
-        bool sheds;
-
-    protected:
-        string favFood;
-        int intell;
-        int aggro;
-        int courage;
-        int play;
-        int strength;
-        string specialSkill;
-        int kidFriendly;
-        int commSkills;
-        int houseTrained;
-        int trust;
-        int curiosity;
-
+	private:
+		string name;
+		int age;
+		string gender;
+		string address;
+		string climate;
+		int pNumber;
 };
 
-
-#endif // ANIMAL_H
+#endif // CLIENT_H
